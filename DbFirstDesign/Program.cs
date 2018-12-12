38b3b73279da84035a230bbcb0a549e3b1914e51
Ryan Nicolay <ryannicolay@gmail.com>
2018-12-12 13:58:20 -0500

    Design a Database Model with Database first design

--- DbFirstDesign/Program.cs
+++ DbFirstDesign/Program.cs
@@ -10,6 +10,13 @@ namespace DbFirstDesign
     {
         static void Main(string[] args)
         {
+            var vidzyDbFirstContext = new VidzyDbFirstContext();
+
+            //vidzyDbFirstContext.AddVideo("Video 1", DateTime.Today.AddDays(-2), "Horror");
+            //vidzyDbFirstContext.AddVideo("Video 2", DateTime.Today.AddDays(-3), "Action");
+            //vidzyDbFirstContext.AddVideo("Video 3", DateTime.Today.AddDays(-4), "Comedy");
+            //vidzyDbFirstContext.AddVideo("Video 4", DateTime.Today.AddDays(-4), "Comedy");
+            vidzyDbFirstContext.AddVideo("Video 5", DateTime.Today.AddDays(-4), "Comedy", (byte)Classification.Platinum);
         }
     }
 }
